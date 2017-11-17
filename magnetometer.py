f8619656f838fc7802db76a9a61ff4eb1a70aa11
Ron Popov <DirtySlowpoke@gmail.com>
2017-11-17 12:43:33 +0000

    Create magnetometer.py

---
+++ magnetometer.py
@@ -0,0 +1,11 @@
+# Returns the current X axis measurment
+def GetX():
+	return "x"
+
+# Returns the current Y axis measurment
+def GetY():
+	return "y"
+
+# Returns the current Z axis measurment
+def GetZ():
+	return "z"
