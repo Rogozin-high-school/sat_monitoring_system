1ad8b6681f73d658cdc1f92246c3affc39ed93f4
ron-popov <DirtySlowpoke@gmail.com>
2017-11-18 17:54:35 +0200

    The server also logs the magnetometer readings now

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 # ------------ import statements
 import socket
 import hmc5883l
+import time
+import thread
 
 # ------------ variables
 HOST, PORT = '', 8080
 magnetometer = hmc5883l.hmc5883l()
-
+f = open('log', 'a+')
+LOG_DELAY_MS = 500
+    
 # ------------ methods
 # TODO : Implement this methods
+# writes data to the log file
+def WriteToLog(text):
+    currentTime = time.strftime("%d/%m/%Y:%H:%M:%S")
+    f.write(currentTime + '|' + text + '\n')
+
 # returns the current x magnetometer reading
 def GetX():
     return 'X'
@@ -21,28 +30,48 @@ def GetZ():
     return 'Z'
 
 
+# ------------ thread
+# this other thread will create a log file
+def loggingThread(delay):
+    while True:
+        try:
+            ValueX = GetX()
+            ValueY = GetY()
+            ValueZ = GetZ()
+            WriteToLog('X:' + GetX() + ' Y:' + GetY() + ' Z:' + GetZ())
+            print("Logged data")
+            time.sleep(delay/1000.0)
+        except KeyboardInterrupt:
+            break
+
+
 # ------------ main code
+# start logging thread
+thread.start_new_thread(loggingThread, (LOG_DELAY_MS,))
+
+# start http server
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 print 'Serving HTTP on port %s ...' % PORT
+
 while True:
-	try :
-		client_connection, client_address = listen_socket.accept()
-		request = client_connection.recv(1024)
-		print request
+    try :
+        client_connection, client_address = listen_socket.accept()
+        request = client_connection.recv(1024)
+        print request
 
-		http_response = "HTTP/1.1 200 OK" + "\n\n"
-		http_response += "<h1>Satellite Monitoring System</h1>" + "\n"
-		http_response += "X : " + GetX() + "</br>"
-		http_response += "Y : " + GetY() + "</br>"
-		http_response += "Z : " + GetZ() + "</br>"
+        http_response = "HTTP/1.1 200 OK" + "\n\n"
+        http_response += "<h1>Satellite Monitoring System</h1>" + "\n"
+        http_response += "X : " + GetX() + "</br>"
+        http_response += "Y : " + GetY() + "</br>"
+        http_response += "Z : " + GetZ() + "</br>"
 
-		
-                client_connection.sendall(http_response)
-		client_connection.close()
-	except KeyboardInterrupt:
-		break
+        
+        client_connection.sendall(http_response)
+        client_connection.close()
+    except KeyboardInterrupt:
+        break
 
 print "Ending server"
