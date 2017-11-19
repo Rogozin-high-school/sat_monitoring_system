d75353f86901fbb0399cb8b27ae646591333ecb3
ron-popov <DirtySlowpoke@gmail.com>
2017-11-19 18:44:27 +0200

    Fixes issue #1

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ import thread
 # ------------ variables
 HOST, PORT = '', 8080
 magnetometer = hmc5883l.hmc5883l()
-f = open('log', 'a+')
-LOG_DELAY_MS = 500
+f = open('log', 'ra+')
+LOG_DELAY_MS = 1000
     
 # ------------ methods
 # TODO : Implement these methods
 # Writes data to the log file
 def write_to_log(text):
     current_time = time.strftime("%d/%m/%Y-%H:%M:%S")
+    f = open('log', 'a+')
     f.write(current_time + '|' + text + '\n')
     f.flush()
+    f.close()
+
+def read_from_log():
+    f=open('log', 'r+')
+    data = f.readlines()
+    f.close()
+    return data
 
 # Returns the current x magnetometer reading
 def get_x():
@@ -42,7 +50,6 @@ def logging_thread(delay):
             write_to_log('X:' + get_x() + ' Y:' + get_y() + ' Z:' + get_z())
             time.sleep(delay/1000.0)
         except KeyboardInterrupt:
-            f.close()
             break
 
 
@@ -61,26 +68,27 @@ while True:
     try :
         client_connection, client_address = listen_socket.accept()
         request = client_connection.recv(1024)
-        path = request.split(' ')[1][1:]
-        print path
-        http_response = "HTTP/1.1 200 OK" + "\n\n"
-        if path == "log":
-            # prints the log
-            print f
-            for line in f:
-                print line
-                http_response += line
-        elif path == "measure":
-            # return the current measurments
-            http_response += "X : " + get_x() + "\n"
-            http_response += "Y : " + get_y() + "\n"
-            http_response += "Z : " + get_z() + "\n"
-
         
-        client_connection.sendall(http_response)
-        client_connection.close()
+        request_split = request.split(' ')
+        if len(request_split) > 1:
+            path = request_split[1][1:]
+            http_response = "HTTP/1.1 200 OK" + "\n\n"
+            if path == "log":
+                # writes the log to the http_response
+                for line in read_from_log():
+                    http_response += line
+            elif path == "measure":
+                # writes the current measurments to the http_response
+                http_response += "X : " + get_x() + "\n"
+                http_response += "Y : " + get_y() + "\n"
+                http_response += "Z : " + get_z() + "\n"
+            else :
+                http_response += "Command unknown\n"
+                http_response += "Available commands are 'log' and 'measure'"
+
+            client_connection.sendall(http_response)
+            client_connection.close()
     except KeyboardInterrupt:
-        f.close()
         break
 
 print "Ending server"
