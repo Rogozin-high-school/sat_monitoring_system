1e21be972abe521224016a70ee22a72eb73caf57
ron-popov <DirtySlowpoke@gmail.com>
2017-11-21 23:15:34 +0200

    Improved thread safety

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import threading
 HOST, PORT = '', 8080
 magnetometer = hmc5883l.hmc5883l()
 LOG_DELAY_MS = 1000
-lock = threading.Lock()
+log_lock = threading.Lock()
+magnetometer_lock = threading.Lock()
 should_log = True
 
 # ------------ methods
@@ -51,12 +52,17 @@ class log_thread(threading.Thread):
     def run(self):
         while should_log:
             try:
-                lock.acquire()
+                # reading data from the magnetometer
+                magnetometer_lock.acquire()
                 value_x = get_x()
                 value_y = get_y()
                 value_z = get_z()
+                magnetometer_lock.release()
+                
+                # writing to the log file
+                log_lock.acquire()
                 write_to_log('X:' + get_x() + ' Y:' + get_y() + ' Z:' + get_z())
-                lock.release()
+                log_lock.release()
                 time.sleep(LOG_DELAY_MS/1000.0)
             except KeyboardInterrupt:
                 break
@@ -85,17 +91,17 @@ while True:
             http_response = "HTTP/1.1 200 OK" + "\n\n"
             if path == "log":
                 # writes the log to the http_response
-                lock.acquire()
+                log_lock.acquire()
                 for line in read_from_log():
                     http_response += line
-                lock.release()
+                log_lock.release()
             elif path == "measure":
                 # writes the current measurments to the http_response
-                lock.acquire()
+                magnetometer_lock.acquire()
                 http_response += "X : " + get_x() + "\n"
                 http_response += "Y : " + get_y() + "\n"
                 http_response += "Z : " + get_z() + "\n"
-                lock.release()
+                magnetometer_lock.release()
             else :
                 http_response += "Command unknown\n"
                 http_response += "Available commands are 'log' and 'measure'"
