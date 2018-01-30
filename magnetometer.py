498e7b440892ad1ccd0ebc793dc367f0d886bf21
shaiKemhi <shaikimhi221199@gmail.com>
2018-01-30 10:02:58 +0200

    updated magnetometer to work with mpu9250 sensor (might work with mpu9265)

--- magnetometer.py
+++ magnetometer.py
@@ -1,19 +1,22 @@
 import random
+from mpu9250.mpu9250 import mpu9250
 
 class magnetometer:
     def __init__(self):
         self.ron = True
+        self.sensor = mpu9250()
+
 
     # ALL OF THESE METHODS SHOULD NOT BE USED ANYWHERE
     # EXCEPT FOR THE magnetomterMT CLASS !!!
     def get_x(self):
-        return random.randint(0,100)/100.0
+        return str(self.sensor.read_xyz()[0])
 
     def get_y(self):
-        return random.randint(0,100)/100.0
+        return  str(self.sensor.read_xyz()[1])
 
     def get_z(self):
-        return random.randint(0,100)/100.0
+        return str(self.sensor.read_xyz()[2])
 
     def get_axes(self):
-        return (self.get_x() ,self.get_y() ,self.get_z())
+        return self.sensor.read_xyz()
