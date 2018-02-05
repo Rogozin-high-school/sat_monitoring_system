cbf2a50ba5ce8babb908c6844940f14b806d3b9b
ron-popov <DirtySlowpoke@gmail.com>
2018-02-05 02:42:10 +0000

    Magnetometer works !

--- Modules/Magnetometer/unitTest.py
+++ Modules/Magnetometer/unitTest.py
@@ -5,7 +5,4 @@ magnetometer = mpu9250.mpu9250()
 
 while True:
     print "Magnetometer" + str(magnetometer.mag)
-    print "Aceleromter" + str(magnetometer.accel)
-    print "Gyro" + str(magnetometer.gyro)
-    print " ------------------- "
     time.sleep(0.1)
