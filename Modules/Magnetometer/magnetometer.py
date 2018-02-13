24d8db54dcd2176ac6fcb55069ced7b5982002bd
ron-popov <ron.popov@hotmail.com>
2018-02-13 20:10:35 +0200

    Minor tweaks to magnetometer.py

--- Modules/Magnetometer/magnetometer.py
+++ Modules/Magnetometer/magnetometer.py
@@ -1,9 +1,7 @@
 from .. import config
 
-if not config.HAS_HARDWARE:
+if config.HAS_HARDWARE:
     from . import mpu9250
-else:
-    import random
 
 class magnetometer:
     """A class for reading data from the magnetometer sensor"""
@@ -11,7 +9,7 @@ class magnetometer:
     def __init__(self):
         """Initialises the sensor object if the
         magnetometer is connected"""
-        if not config.HAS_HARDWARE:
+        if config.HAS_HARDWARE:
             self.sensor = mpu9250.mpu9250()
 
     # Reading data from magnetometer
@@ -32,7 +30,7 @@ class magnetometer:
     def __get_axes__(self):
         """Reads magnetic field in a form of a tuple containing
         x, y and z axis values"""
-        if not config.HAS_HARDWARE:
+        if config.HAS_HARDWARE:
             return self.sensor.read_xyz()
         else:
             return (0,0,0)        
