d723862abf6e5fbc38763c4f382b09c054734ccb
ron-popov <ron.popov@hotmail.com>
2018-02-14 11:16:37 +0200

    Added length calculation to magnetometer test

--- Modules/Tests/magnetometer.py
+++ Modules/Tests/magnetometer.py
@@ -1,4 +1,6 @@
 from ..Magnetometer import magnetometerMT
+import math
+
 
 sensor = magnetometerMT.magnetometerMT()
 
@@ -7,6 +9,11 @@ shouldRun = True
 while shouldRun:
     try :
         axes = sensor.get_axes()
-        print ("%8.3f,%8.3f,%8.3f" % (axes[0], axes[1], axes[2]))
+        x = axes[0]
+        y = axes[1]
+        z = axes[2]
+        length = math.sqrt(x*x + y*y + z*z)
+
+        print ("x:%8.3f    y:%8.3f    z:%8.3f    length:" % (x, y, z, length))
     except KeyboardInterrupt:
         shouldRun = False
