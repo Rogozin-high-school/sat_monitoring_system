cd010908897ec12ee418c11e31e03d9ba245457e
Yotam Salmon <yotam.salmon@gmail.com>
2018-02-17 20:18:34 +0200

    Add operator spacing in Vector2D

--- Modules/Vector/Vector2D.py
+++ Modules/Vector/Vector2D.py
@@ -1,12 +1,17 @@
 #/usr/bin/python3
 from math import sqrt
+
 class vector2D:
-    def __init__(self,x:float=0,y:float=0):
+
+    def __init__(self, x : float = 0, y : float = 0):
         self.x = x
         self.y = y
-    def distance_to(self,vector):
-        return sqrt((self.x-vector.x)**2+(self.y-vector.y)**2)
+
+    def distance_to(self, vector):
+        return sqrt((self.x - vector.x) ** 2 + (self.y - vector.y) ** 2)
+
     def length(self):
-        return sqrt(self.x**2+self.y**2)
-    def __add__(self,vector):
-        return vector2D(self.x+vector.x,self.y+vector.y)
+        return sqrt(self.x ** 2 + self.y ** 2)
+
+    def __add__(self, vector):
+        return vector2D(self.x + vector.x, self.y + vector.y)
