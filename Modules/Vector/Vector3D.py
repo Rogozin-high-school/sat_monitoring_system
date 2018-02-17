bd386c7113f07890566f83970b819529169314d3
Yotam Salmon <yotam.salmon@gmail.com>
2018-02-17 20:19:52 +0200

    Added spacing in Vector3D

--- Modules/Vector/Vector3D.py
+++ Modules/Vector/Vector3D.py
@@ -1,13 +1,18 @@
 #/usr/bin/python3
 from math import sqrt
+
 class vector3D:
-    def __init__(self,x:float=0,y:float=0,z:float=0):
+
+    def __init__(self, x : float = 0, y : float = 0, z : float = 0):
         self.x = x
         self.y = y
         self.z = z
-    def distance_to(self,vector)->int:
-        return sqrt((self.x-vector.x)**2+(self.y-vector.y)**2+(self.z-vector.z)**2)
-    def length(self)->int:
-        return sqrt(self.x**2+self.y**2+self.z**2)
-    def __add__(self,vector):
-        return vector3D(self.x+vector.x,self.y+vector.y,self.z+vector.z)
+
+    def distance_to(self, vector) -> int:
+        return sqrt((self.x - vector.x) ** 2 + (self.y - vector.y) ** 2 + (self.z - vector.z) ** 2)
+
+    def length(self) -> int:
+        return sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+    def __add__(self, vector):
+        return vector3D(self.x + vector.x, self.y + vector.y, self.z + vector.z)
