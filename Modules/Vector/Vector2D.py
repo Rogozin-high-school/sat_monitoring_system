45539b0d7a37d2d4c41eaf19fe19b10c92f63caa
shaiKemhi <shaikimhi221199@gmail.com>
2018-02-18 21:12:59 +0200

    removed unstable type declaration|

--- Modules/Vector/Vector2D.py
+++ Modules/Vector/Vector2D.py
@@ -7,13 +7,13 @@ class vector2D:
     def __init__(self, x : float = 0, y : float = 0):
         self.x = x
         self.y = y
-    def distance_to(self, vector : vector2D) -> float:
+    def distance_to(self, vector) -> float:
         return sqrt((self.x - vector.x) ** 2 + (self.y - vector.y) ** 2)
 
     def length(self) -> float:
         return sqrt(self.x ** 2 + self.y ** 2)
 
-    def __add__(self, vector : vector2D):
+    def __add__(self, vector):
         return vector2D(self.x + vector.x, self.y + vector.y)
 		
 	def __mul__(self,vector):
