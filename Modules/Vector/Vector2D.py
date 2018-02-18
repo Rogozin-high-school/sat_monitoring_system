0d4dcf7e106388fcef2eb73e0d719fb38acb695b
shaiKemhi <shaikimhi221199@gmail.com>
2018-02-18 21:19:45 +0200

    changed severals problems

--- Modules/Vector/Vector2D.py
+++ Modules/Vector/Vector2D.py
@@ -7,7 +7,7 @@ class vector2D:
     def __init__(self, x : float = 0, y : float = 0):
         self.x = x
         self.y = y
-    def distance_to(self, vector) -> float:
+    def distance_to(self, vector:vector2D) -> float:
         return sqrt((self.x - vector.x) ** 2 + (self.y - vector.y) ** 2)
 
     def length(self) -> float:
@@ -16,12 +16,9 @@ class vector2D:
     def __add__(self, vector):
         return vector2D(self.x + vector.x, self.y + vector.y)
 		
-	def __mul__(self,vector):
-		vector2 = vector2D()
-        return vector2
 		
-	def scalar_mul(self,vector):
-		return self.length() * vector.length() * math.cos(self.angle(vector))
-	def angle(self,vector):
-		return math.acos((self.x * vector.x + self.y * vector.y) / (self.length() * vector.length()))
+    def scalar_mul(self,vector):
+        return self.length() * vector.length() * math.cos(self.angle(vector))
+    def angle(self,vector):
+        return math.acos((self.x * vector.x + self.y * vector.y) / (self.length() * vector.length()))
 
