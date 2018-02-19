19fcb81c69c84c7fc68d7d197c8fbf82074b435a
shaiKemhi <shaikimhi221199@gmail.com>
2018-02-19 15:00:37 +0200

    added types sepcification

--- Modules/Vector/Vector3D.py
+++ Modules/Vector/Vector3D.py
@@ -8,17 +8,17 @@ class vector3D:
         self.x = x
         self.y = y
         self.z = z
-    def distance_to(self, vector) -> float:
+    def distance_to(self, vector:'vector3D') -> float:
         return sqrt((self.x - vector.x) ** 2 + (self.y - vector.y) ** 2 + (self.z - vector.z) ** 2)
 
     def length(self) -> float:
         return sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
-    def __add__(self, vector):
+    def __add__(self, vector:'vector3D'):
         return vector3D(self.x + vector.x, self.y + vector.y, self.z + vector.z)
-    def scalar_mul(self,vector):
+    def scalar_mul(self,vector:'vector3D'):
         return self.length() * vector.length() * math.cos(self.angle(vector))
-    def angle(self,vector):
+    def angle(self,vector:'vector3D'):
         return math.acos((self.x * vector.x + self.y * vector.y + self.z * vector.z) / (self.length() * vector.length()))
 
 
