03207fef2e9a66c62d785f367b2bee0c1809cc9b
shaiKemhi <shaikimhi221199@gmail.com>
2018-03-11 12:24:15 +0200

    fixed and added spaces in Field_TO_Currnet

--- Modules/Control/Field_To_Current.py
+++ Modules/Control/Field_To_Current.py
@@ -3,7 +3,7 @@ AMOUNT_OF_WRAPS = 100
 RADIUS = 0.09
 
 def field_to_current(field:Array)->float:
-    return BaseException/(AMOUNT_OF_WRAPS*math.pi*RADIUS**2)
+    return field / (AMOUNT_OF_WRAPS * math.pi * RADIUS**2)
 
-def Calc_Field(current:float)->Array:
-    return current*AMOUNT_OF_WRAPS*math.pi*RADIUS**2
+def Calc_Field(current:float)->float:
+    return current * AMOUNT_OF_WRAPS * math.pi * RADIUS**2
