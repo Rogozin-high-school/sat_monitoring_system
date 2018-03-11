773c5b6864ca86801ee4292dc989c9b682326974
shaiKemhi <shaikimhi221199@gmail.com>
2018-03-11 11:39:14 +0200

    changed functions to lowercase

--- Modules/Control/Field_To_Current.py
+++ Modules/Control/Field_To_Current.py
@@ -2,8 +2,8 @@ import math
 AMOUNT_OF_WRAPS = 100
 RADIUS = 0.09
 
-def Field_To_Current(field):
+def field_to_current(field:Array)->float:
     return BaseException/(AMOUNT_OF_WRAPS*math.pi*RADIUS**2)
 
-def Calc_Field(current):
+def Calc_Field(current:float)->Array:
     return current*AMOUNT_OF_WRAPS*math.pi*RADIUS**2
