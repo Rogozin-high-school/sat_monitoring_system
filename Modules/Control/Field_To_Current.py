7b3a48c44ddaa21d091f85b5ebb6c9afe79b1016
shaiKemhi <shaikimhi221199@gmail.com>
2018-03-12 15:58:24 +0200

    Added current to voltage and voltage to current convertion:

--- Modules/Control/Field_To_Current.py
+++ Modules/Control/Field_To_Current.py
@@ -1,9 +1,17 @@
 import math
+import numpy
 AMOUNT_OF_WRAPS = 100
-RADIUS = 0.09
+RADIUS = 0.09 #(meter)
+RESISTANCE = 170 #(ohm)
 
-def field_to_current(field:Array)->float:
+def field_to_current(field:numpy.ndarray)->float:
     return field / (AMOUNT_OF_WRAPS * math.pi * RADIUS**2)
 
-def Calc_Field(current:float)->float:
+def calc_field(current:float)->float:
     return current * AMOUNT_OF_WRAPS * math.pi * RADIUS**2
+
+def current_to_voltage(current:float)->float:
+    return current*RESISTANCE
+
+def voltage_to_current(voltage:float)->float:
+    reutrn voltage/RESISTANCE
