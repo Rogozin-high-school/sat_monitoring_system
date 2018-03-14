b56add8ca7ab2412d5b917c1d063f367f828a36d
unknown <shaikimhi221199@gmail.com>
2018-03-14 16:33:12 +0200

    updated Control Tester

--- Tests/Control.py
+++ Tests/Control.py
@@ -1,14 +1,14 @@
-from ..Communication import satprot
+from ..Modules.Satprot import satprot
 from ..Modules.Control import Control
 import time
-con = satprot.connection("192.168.2.104",14944)
+con = satprot.Connection(input("Enter target ip: "),14944)
 while(True):
     time.sleep(0.05)
     t = time.time()
     direction = (Control.get_angle(t))
     msg = satprot.OutMsg(1,0)
-    msg.add_float(direction[0])
-	msg.add_float(direction[1])
-	msg.add_float(direction[2])
-    print("x:" + direction[0] + " y:" + direction[1] + " z:" + direction[2])
+    msg.add_float(direction)
+    msg.add_float(0)
+    msg.add_float(0)
+    print("angle: "+str(direction))
     con.send(msg)
