b35bd7ad619bf7b96ca45480a64be96d576b7d25
ron-popov <ron.popov@hotmail.com>
2018-03-20 13:27:53 +0200

    Bias calculation script

---
+++ Tests/calc_bias_magnetometer.py
@@ -0,0 +1,20 @@
+from ..Modules.Magnetomter import magnetometerMT
+
+sensor = magnetometerMT.magnetometerMT()
+
+# A 2D array of size 3 by 2, that would store the max and min values for each axis
+bias = [[None, None], [None, None], [None, None]]
+axis_names = ['x', 'y', 'z']
+
+try:
+    while True:
+        field = sensor.mag
+        for axis in bias:
+            if axis[0] == None or field[axis] < axis[0]:
+                axis[0] = field[axis]
+            elif axis[1] == None or field[axis] > axis[1]:
+                axis[1] = field[axis]
+except KeyboardInterrupt:
+    for axis in bias:
+        bias = (axis[0] + axis[1]) / 2
+        print(axis_names[axis] + " : " + bias)
