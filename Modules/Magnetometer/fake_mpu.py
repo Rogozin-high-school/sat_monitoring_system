396d69148748c228ceac9369242cbe06545dc832
shaiTheKimhi <shaikimhi221199@gmail.com>
2018-03-20 12:07:15 +0200

    added circular field module

--- Modules/Magnetometer/fake_mpu.py
+++ Modules/Magnetometer/fake_mpu.py
@@ -1,6 +1,28 @@
+import numpy
+import math
+import time
+from . import config
 class fake_mpu:
     def __init__(self):
         n = 0#Empy statement
     #TODO : add real field simulation
     def read_xyz():
         return [0,0,0]
+
+def circular_field(angle:float):
+	angle = math.radians(angle)
+	a = math.sin(angle)
+	b = math.cos(angle)
+	x = None
+	y = None
+	if(b > config.MIN_SIZE or b < -config.MIN_SIZE):
+		x = 1
+		y = -a/b
+		length = math.sqrt(x**2 + y**2)
+		x = x / length
+		y = y / length
+		return [x,y]
+	else:
+		x = 0
+		y = a * FIELD_SIZE
+	return [x,y]
