b0786f705c60381250aa428c21756b07c7274d85
ron-popov <ron.popov@hotmail.com>
2018-03-20 15:34:22 +0200

    Fixed magnetometer sim

--- Modules/Magnetometer/fake_magnetometer.py
+++ Modules/Magnetometer/fake_magnetometer.py
@@ -2,19 +2,24 @@ import numpy
 import math
 import time
 from . import config
-class fake_mpu:
+
+class fake_magnetometer:
+
     def __init__(self):
-        n = 0#Empy statement
+        pass
+
     #TODO : add real field simulation
     def read_xyz():
         return [0,0,0]
 
 def circular_field(angle:float):
+
 	angle = math.radians(angle)
 	a = math.sin(angle)
 	b = math.cos(angle)
 	x = None
 	y = None
+
 	if(b > config.MIN_SIZE or b < -config.MIN_SIZE):
 		x = 1
 		y = -a/b
@@ -24,5 +29,6 @@ def circular_field(angle:float):
 		return [x,y]
 	else:
 		x = 0
-		y = a * FIELD_SIZE
+		y = a * FIELD_SIZES
+
 	return [x,y]
