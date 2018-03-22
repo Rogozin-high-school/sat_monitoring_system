2c04131fc5e31b522ec0bb86ca58f5dc31cbb184
ron-popov <ron.popov@hotmail.com>
2018-03-22 17:34:38 +0200

    Restructured magnetometer_factory

--- Modules/Magnetometer/magnetometer_factory.py
+++ Modules/Magnetometer/magnetometer_factory.py
@@ -1,16 +1,17 @@
 from . import config
 
+
 if config.MAGNETOMETER_CONNECTED:
     from . import mpu9250
+    
+    def __init_hardware():
+        return mpu9250.MPU9250()
+
+    initialize = __init_hardware
 else:
     from . import fake_magnetometer
 
-
-
-#this function returns the mpu9250 hardware module object if the hardware is connected.
-#otherwise returns and object that simulates hardware
-def initialize():
-    if(config.MAGNETOMETER_CONNECTED == True):
-        return mpu9250.MPU9250()
-    else:
+    def __init_fake_hardware():
         return fake_magnetometer.fake_magnetometer()
+    
+    initialize = __init_fake_hardware
