62c9c7e6233e0e8d6e710f897ab11d25f33416be
unknown <shaikimhi221199@gmail.com>
2018-03-25 12:18:55 +0300

    extanded documantation

--- Modules/Control/Scale_Convert.py
+++ Modules/Control/Scale_Convert.py
@@ -1,8 +1,15 @@
 import numpy
 
-def convert(tilt:numpy.ndarray ,target:numpy.ndarray) ->numpy.ndarray:
+'''
+Input:
+    tilt - the tilt of the wanted scale in comparison to the given scale
+    vector - the given vector for the first scale
+Output:
+    the given vector for the 2nd scale
+'''
+def convert(tilt:numpy.ndarray ,vector:numpy.ndarray) ->numpy.ndarray:
     #this function converts between different scales for a known tilt
-    x = target[0]-tilt[0]
-    y = target[1] - tilt[1]
-    z = target[2] - tilt[2]
+    x = vector[0]-tilt[0]
+    y = vector[1] - tilt[1]
+    z = vector[2] - tilt[2]
     return numpy.ndarray([x,y,z])
