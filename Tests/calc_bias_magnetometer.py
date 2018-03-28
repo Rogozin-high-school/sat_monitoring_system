5bc8ed5a57c77207096b83ad3538eb0fd6198a53
ron-popov <ron.popov@hotmail.com>
2018-03-28 12:17:53 +0300

    Fixed bias calculation script

--- Tests/calc_bias_magnetometer.py
+++ Tests/calc_bias_magnetometer.py
@@ -5,15 +5,28 @@ sensor = magnetometer_factory.initialize()
 # A 2D array of size 3 by 2, that would store the max and min values for each axis
 bias = [[None, None], [None, None], [None, None]]
 axis_names = ['x', 'y', 'z']
+initialized = False
+
 
 try:
     while True:
         field = sensor.readMagnet()
-        for axis in bias:
-            if axis[0] == None or field[axis] < axis[0]:
-                axis[0] = field[axis]
-            elif axis[1] == None or field[axis] > axis[1]:
-                axis[1] = field[axis]
+
+        if not initialized:
+            bias[0][0] = field['x']
+            bias[0][1] = field['x']
+
+            bias[1][0] = field['y']
+            bias[1][1] = field['y']
+
+            bias[2][0] = field['z']
+            bias[2][1] = field['z']
+
+        else:
+            for axis in range(len(axis)):
+                bias[axis][0] = min(field[axis_names[axis]], bias[axis][0])
+                bias[axis][1] = max(field[axis_names[axis]], bias[axis][0])
+
 except KeyboardInterrupt:
     for axis in bias:
         bias = (axis[0] + axis[1]) / 2
