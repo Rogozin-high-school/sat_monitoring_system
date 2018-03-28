51eb4b257d82854edb7c533ab6f0d7b5334838e6
ron-popov <ron.popov@hotmail.com>
2018-03-28 11:31:42 +0300

    Rewrote magnetometer test

--- Tests/magnetometer.py
+++ Tests/magnetometer.py
@@ -9,13 +9,9 @@ shouldRun = True
 while shouldRun:
     try :
         axes = sensor.get_axes()
-        x = axes[0]
-        y = axes[1]
-        z = axes[2]
-        length = math.sqrt(x*x + y*y + z*z)
-
-        print ("x:%8.3f    y:%8.3f    z:%8.3f    length:%8.3f" % (x, y, z, length))
-
+        
+        print(axes)
+        
         time.sleep(0.125)
     except KeyboardInterrupt:
         shouldRun = False
