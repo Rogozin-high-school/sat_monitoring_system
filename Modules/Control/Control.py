5bde3b65e7e1eabcecc36c394b7724dcb192b136
shaiKemhi <shaikimhi221199@gmail.com>
2018-04-15 18:33:40 +0300

    Secondary thread that changes MT values

--- Modules/Control/Control.py
+++ Modules/Control/Control.py
@@ -1,4 +1,8 @@
-from ..Magnetometer import magnetometerMT
+from ..Magnetometer import magnetometer_factory
+from ..Magnetorquer import magnetorquerMT
+from ..Accelerometer import accelerometer_factory
+from ..Control import Scale_Convert
+from threading import Thread
 import math
 import numpy
 import time
@@ -9,24 +13,37 @@ __circle_time = 90 * 60
 
 class controller:
     def __init__(self,mode=1):
-        self.magnetometer = magnetometerMT()
+        self.magnetometer = magnetometer_factory.initialize()
+        self.gyro = accelerometer_factory.initialize()
+        self.magnetorquer = magnetorquerMT.magnetorquerMT()
         self.time = time.time()
-        #TODO : Add a secondary thread that changes calls setControl
-        
-
+        # secondary thread that changes calls setControl
+        thread = Thread(target = set_control ,args = (2,))
+        thread.start()
+        thread.join()
+	
+    def move(self, mode = 2):
+        while(True):
+            arr = setControl(mode = mode)
+            self.magnetorquer.set_x(arr[0])
+            self.magnetorquer.set_y(arr[1])
+            self.magnetorquer.set_z(arr[2])
+            time.sleep(0.1)
     #TODO: Add offset for meneuvering to certain locations on earth
     def set_control(self, mode=1, offset=[0,0]):
-        #makes the direction change for the measured depolar magnetic field (cos function value)
+        # makes
         if(mode == 1):
             field = self.magnetometer.get_axes()
             timer = (self.time-time.time()) % (90*60)
             torque = get_angle_vector(get_angle(timer))
             z = (field[1]*torque[0] - filed[0]*torque[1]) / (field[2]*torque[1] - field[1]*torque[2])
             y = (-field[0] - z * field[2]) / field[1]
-            return np.array([1,y,z])
+            arr = np.ndarray([1,y,z])
+            tilt = np.ndarray([self.gyro.accel()])
+            return arr - tilt
         #makes the direction change for circular magnetic field, in order to go by the field
         elif(mode == 2):
-            return np.array([1,1,1])
+            return np.array([0,0,1])
 
 ''''
 returns the ratio of y1 to x1 where (x1;y1) is the vertical 2D vector to (x;y) vector
