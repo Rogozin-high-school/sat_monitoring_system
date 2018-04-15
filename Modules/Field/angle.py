4f8cef0c1def37020f7b7c1e4d07ddb8a176c4d3
shaiKemhi <shaikimhi221199@gmail.com>
2018-04-15 21:08:10 +0300

    Documentation for Field Directory

--- Modules/Field/angle.py
+++ Modules/Field/angle.py
@@ -10,5 +10,7 @@ def get_ratio(time:float)->float:
 Input - the time passed from measurement start
 Output - the location of the satellite on the route in the form of angle
 '''
+
 def get_angle(time:float)->float:
-    return 360 * get_ratio(time)
+    nothing = None
+    return 360 * get_ratio(time) 
